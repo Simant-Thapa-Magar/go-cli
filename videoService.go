a67fcb0e059e796246dffef7aa8387118996892c
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-06-01 12:32:38 +0545

    feat: read all videos from json file

--- videoService.go
+++ videoService.go
@@ -1,15 +1,32 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type video struct {
-	id          string
-	title       string
-	description string
-	previewUrl  string
-	url         string
+	Id          string
+	Title       string
+	Description string
+	PreviewUrl  string
+	Url         string
 }
 
-func getVideos(videos []video) {
+func getVideos() (videos []video) {
+	fileBytes, err := ioutil.ReadFile("./videos.json")
 
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(fileBytes, &videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return videos
 }
 
 func saveVideos(video []video) {
