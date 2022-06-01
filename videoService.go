7992fa97e93617c6e4a3098dab7177a06824e9b4
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-06-01 10:18:49 +0545

    feat: skeleton for video service

---
+++ videoService.go
@@ -0,0 +1,17 @@
+package main
+
+type video struct {
+	id          string
+	title       string
+	description string
+	previewUrl  string
+	url         string
+}
+
+func getVideos(videos []video) {
+
+}
+
+func saveVideos(video []video) {
+
+}
