6ccbe8c35c6022edaa96fd91eaa0906440b62ee8
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-06-01 10:43:52 +0545

    feat: skeleton for get command

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// command for getting videos
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+
+	// input for get videos command
+	// all videos
+	getAll := getCmd.Bool("all", false, "Get all vidos")
+
+	// video by id
+	getId := getCmd.String("id", "", "Video ID")
+
+	// command args validation
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'get' or 'save' command")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "get":
+		// handle get command
+		handleGet(getCmd, getAll, getId)
+		break
+	case "save":
+		// handle save command
+	default:
+	}
+}
+
+func handleGet(getCmd *flag.FlagSet, getAll *bool, getId *string) {
+	fmt.Println("I will handle get command")
 }
